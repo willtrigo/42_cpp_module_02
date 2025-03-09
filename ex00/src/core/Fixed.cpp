c62a04072658929463112bc9292e392da5669dcc
willtrigo <willtrigo@gmail.com>
2025-03-09 19:54:47 -0300

    Fix: change the m_raw to m_value and applying bets conventions in Fixed.hpp  Changes to be committed: 	modified:   ex00/src/core/Fixed.cpp 	modified:   ex00/src/core/Fixed.hpp

--- ex00/src/core/Fixed.cpp
+++ ex00/src/core/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 17:50:57 by dande-je          #+#    #+#             */
-/*   Updated: 2025/03/09 14:35:19 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/03/09 19:22:29 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,37 +14,37 @@
 #include "utils/TerminalColor.hpp"
 #include <iostream>
 
-Fixed::Fixed() : m_raw(RAW_DEFAULT), m_color(TerminalColor::getInstance()), m_str_color(WHITE), m_bg_color(BG_RESET)  {
-  std::cout << this->m_color.setColor(this->m_bg_color, this->m_str_color, "Default constructor called") << std::endl;
+Fixed::Fixed() : m_value(VALUE_DEFAULT), m_color(TerminalColor::getInstance()),  m_bgColor(BG_RESET), m_strColor(WHITE)  {
+  std::cout << this->m_color.setColor(this->m_bgColor, this->m_strColor, "Default constructor called") << std::endl;
 }
 
 Fixed::Fixed(Fixed const& other) : m_color(TerminalColor::getInstance()) {
   std::cout << this->m_color.setColor(BG_RESET, BLUE, "Copy constructor called") << std::endl;
   *this = other;
-  this->m_str_color = BLUE;
-  this->m_bg_color = BG_RESET;
+  this->m_strColor = BLUE;
+  this->m_bgColor = BG_RESET;
 }
 
 Fixed& Fixed::operator=(const Fixed& other) {
   if (this != &other) {
     std::cout << this->m_color.setColor(BG_RESET, ORANGE, "Copy assignment operator called") << std::endl;
-    this->m_str_color = ORANGE;
-    this->m_bg_color = BG_RESET;
-    this->m_raw = other.getRawBits();
+    this->m_strColor = ORANGE;
+    this->m_bgColor = BG_RESET;
+    this->m_value = other.getRawBits();
   }
   return *this;
 }
 
 Fixed::~Fixed() {
-  std::cout << this->m_color.setColor(this->m_bg_color, this->m_str_color, "Destructor called") << std::endl;
+  std::cout << this->m_color.setColor(this->m_bgColor, this->m_strColor, "Destructor called") << std::endl;
 }
 
 int Fixed::getRawBits() const {
-  std::cout << this->m_color.setColor(this->m_bg_color, this->m_str_color, "getRawBits member function called") << std::endl;
-  return this->m_raw;
+  std::cout << this->m_color.setColor(this->m_bgColor, this->m_strColor, "getRawBits member function called") << std::endl;
+  return this->m_value;
 }
 
 void Fixed::setRawBits(int const raw) {
-  std::cout << this->m_color.setColor(this->m_bg_color, this->m_str_color, "setRawBits member function called") << std::endl;
-  this->m_raw = raw;
+  std::cout << this->m_color.setColor(this->m_bgColor, this->m_strColor, "setRawBits member function called") << std::endl;
+  this->m_value = raw;
 }
--- ex00/src/core/Fixed.hpp
+++ ex00/src/core/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 17:50:54 by dande-je          #+#    #+#             */
-/*   Updated: 2025/03/08 20:23:43 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/03/09 19:27:06 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,24 @@ class Fixed {
  public:
   Fixed();
   Fixed(const Fixed& other);
-  Fixed& operator=(const Fixed& other);
   ~Fixed();
+
+  Fixed& operator=(const Fixed& other);
 
   int getRawBits() const;
   void setRawBits(int const raw);
 
  private:
   enum defaultEnum {
-    RAW_DEFAULT
+    VALUE_DEFAULT
   };
-  int m_raw;
+
   static const int m_fractionalBits = 8;
+
+  int m_value;
   const TerminalColor& m_color;
-  ColorCode m_str_color;
-  ColorCode m_bg_color;
+  ColorCode m_bgColor;
+  ColorCode m_strColor;
 };
 
 #endif // FIXED_HPP
