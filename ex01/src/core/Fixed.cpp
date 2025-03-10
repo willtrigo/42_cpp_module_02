22957ecdd52917029dadf6d82d80ff8873547869
willtrigo <willtrigo@gmail.com>
2025-03-09 21:26:32 -0300

    Fix: applying best convention to large set values in the function  Changes to be committed: 	modified:   ex01/src/core/Fixed.cpp

--- ex01/src/core/Fixed.cpp
+++ ex01/src/core/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/08 21:49:26 by dande-je          #+#    #+#             */
-/*   Updated: 2025/03/09 21:04:50 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/03/09 21:19:09 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,27 @@
 #include <cmath>
 #include <iostream>
 
-Fixed::Fixed() : m_value(VALUE_DEFAULT), m_color(TerminalColor::getInstance()), m_strColor(WHITE), m_bgColor(BG_RESET)  {
+Fixed::Fixed()
+    : m_value(VALUE_DEFAULT),
+      m_color(TerminalColor::getInstance()),
+      m_strColor(WHITE),
+      m_bgColor(BG_RESET)  {
   std::cout << this->m_color.setColor(this->m_bgColor, this->m_strColor, "Default constructor called") << std::endl;
 }
 
-Fixed::Fixed(const int value) : m_value(value << this->m_fractionalBits), m_color(TerminalColor::getInstance()), m_strColor(YELLOW), m_bgColor(BG_RESET) {
+Fixed::Fixed(const int value)
+    : m_value(value << m_fractionalBits),
+      m_color(TerminalColor::getInstance()), 
+      m_strColor(YELLOW),
+      m_bgColor(BG_RESET) {
   std::cout << this->m_color.setColor(this->m_bgColor, this->m_strColor, "Int constructor called") << std::endl;
 }
 
-Fixed::Fixed(const float value) : m_value(roundf(value * (BIT_SHIFT << this->m_fractionalBits))), m_color(TerminalColor::getInstance()), m_strColor(RED), m_bgColor(BG_RESET) {
+Fixed::Fixed(const float value)
+    : m_value(roundf(value * (BIT_SHIFT << m_fractionalBits))),
+      m_color(TerminalColor::getInstance()),
+      m_strColor(RED),
+      m_bgColor(BG_RESET) {
   std::cout << this->m_color.setColor(this->m_bgColor, this->m_strColor, "Float constructor called") << std::endl;
 }
 
