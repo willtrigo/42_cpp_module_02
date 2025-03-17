23bd9914997bda14a087d4dc54119f574244d2e0
willtrigo <willtrigo@gmail.com>
2025-03-17 02:59:15 -0300

    Fix: rename the file as pdf required  Changes to be committed: 	modified:   ex03/Makefile 	renamed:    ex03/src/core/fixed/FixedConstructors.cpp -> ex03/src/core/fixed/Fixed.cpp 	renamed:    ex03/src/core/point/PointConstructor.cpp -> ex03/src/core/point/Point.cpp 	modified:   ex03/src/core/point/bsp.cpp

--- ex03/src/core/point/bsp.cpp
+++ ex03/src/core/point/bsp.cpp
@@ -6,13 +6,15 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/14 17:20:17 by dande-je          #+#    #+#             */
-/*   Updated: 2025/03/14 18:55:36 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/03/17 02:57:24 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core/point/Point.hpp"
 
-static Fixed crossProduct(Point const point, Point const vertexA, Point const vertexB);
+static Fixed crossProduct(Point const point,
+                          Point const vertexA,
+                          Point const vertexB);
 
 bool bsp(Point const a, Point const b, Point const c, Point const point) {
   Fixed product1 = crossProduct(point, a, b);
@@ -28,7 +30,9 @@ bool bsp(Point const a, Point const b, Point const c, Point const point) {
   return all_positive || all_negative;
 }
 
-static Fixed crossProduct(Point const point, Point const vertexA, Point const vertexB) {
+static Fixed crossProduct(Point const point,
+                          Point const vertexA,
+                          Point const vertexB) {
   return (vertexA.getX() - point.getX()) * (vertexB.getY() - point.getY()) -
          (vertexA.getY() - point.getY()) * (vertexB.getX() - point.getX());
 }
